7644086701d9ccb847592d8d7741a5146f715947
JesusLares23 <jesus.lares233383@potros.itson.edu.mx>
2025-02-20 20:06:35 -0700

    agregue una impresón

--- src/main/java/org/itson/gitprubeas/GitPrubeas.java
+++ src/main/java/org/itson/gitprubeas/GitPrubeas.java
@@ -9,5 +9,9 @@ public class GitPrubeas {
 
     public static void main(String[] args) {
         System.out.println("haha");
+        
+        System.out.println("algo");
     }
 }
+
+// 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
