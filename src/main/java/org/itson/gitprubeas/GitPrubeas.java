0f7dc1b1982d48c6480e6f4763118844b3d96863
JesusLares23 <jesus.lares233383@potros.itson.edu.mx>
2025-02-20 19:43:32 -0700

    Primer commit para mandar un mensaje

---
+++ src/main/java/org/itson/gitprubeas/GitPrubeas.java
@@ -0,0 +1,13 @@
+
+package org.itson.gitprubeas;
+
+/**
+ *
+ * @author darkm
+ */
+public class GitPrubeas {
+
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+}
